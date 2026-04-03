c68ee959a5c5f640734e2d138ebc14aa2aa14637
parthrugved <parthrugved@gmail.com>
2026-04-03 14:30:32 +0530

    deployment cmmit

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import os
 
 app = Flask(__name__)
 
-
 uri = os.environ.get("MONGO_URI")
-
 client = MongoClient(uri)
 
 db = client["sareekraft"]
@@ -24,12 +22,15 @@ def index():
         feedback = request.form.get("feedback")
         city = request.form.get("city")
 
+
         if not name or not email or not phone_number or not feedback or not city:
             return render_template("index.html", error="All fields are required ❌", form=request.form)
+
 
         email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
         if not re.match(email_pattern, email):
             return render_template("index.html", error="Invalid email format ❌", form=request.form)
+
 
         if not phone_number.isdigit() or len(phone_number) != 10:
             return render_template("index.html", error="Invalid phone number ❌", form=request.form)
@@ -39,6 +40,9 @@ def index():
 
         if any(char.isdigit() for char in city):
             return render_template("index.html", error="City cannot contain numbers ❌", form=request.form)
+
+        if any(char.isdigit() for char in feedback):
+            return render_template("index.html", error="Feedback cannot contain numbers ❌", form=request.form)
 
         if len(feedback) < 5:
             return render_template("index.html", error="Feedback too short ❌", form=request.form)
